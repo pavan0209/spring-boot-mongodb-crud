5c34dcb0b457fb0aab0091a7f7d1fe7e8bc02eac
pavan0209 <pavanasonawane@gmail.com>
2026-07-12 11:41:06 +0530

    initial commit

---
+++ src/main/java/com/pavan/coding/spring_boot_mongodb_crud/SpringBootMongodbCrudApplication.java
@@ -0,0 +1,13 @@
+package com.pavan.coding.spring_boot_mongodb_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbCrudApplication.class, args);
+	}
+
+}
