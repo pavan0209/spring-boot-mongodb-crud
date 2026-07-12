516c04ac54dca5075cddb2d512065e77d46f4c35
pavan0209 <pavanasonawane@gmail.com>
2026-07-12 12:26:02 +0530

    added Controller and created API add-student

---
+++ src/main/java/com/pavan/coding/spring_boot_mongodb_crud/Controller/AppController.java
@@ -0,0 +1,21 @@
+package com.pavan.coding.spring_boot_mongodb_crud.Controller;
+
+import com.pavan.coding.spring_boot_mongodb_crud.model.Student;
+import com.pavan.coding.spring_boot_mongodb_crud.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/student")
+public class AppController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @PostMapping("/add-student")
+    public ResponseEntity<?> addStudent(@RequestBody Student student) {
+        Student saved = this.studentRepository.save(student);
+        return ResponseEntity.ok(saved);
+    }
+}
